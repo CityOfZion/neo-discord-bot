7983e2eb816bfb214a75e8c85c9f1b560bc3091a
Michael de Wal <michaeldewal@gmail.com>
2017-08-06 02:52:42 +0900

    moved arguments to command line

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const MongoClient = require('mongodb').MongoClient;
+const {mongoUrl, apiKey} = require('minimist')(process.argv.slice(2));
 const Bot = require('./bot');
 
-MongoClient.connect('YOUR_MONGO_DATABASE_URL', function (err, db) {
+MongoClient.connect(mongoUrl, function(err, db) {
   console.log("Connected correctly to server");
-  
-  const discordBot = new Bot(db, client, 'YOUR_BOT_API_KEY');
+  const discordBot = new Bot(db, client, apiKey);
   discordBot.init();
   
 });
