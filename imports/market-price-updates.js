ac566b0dde40a1d57a56046f50700bc912808ae2
Michael de Wal <michaeldewal@gmail.com>
2018-02-21 18:35:16 +0900

    market price update binance

--- imports/market-price-updates.js
+++ imports/market-price-updates.js
@@ -1,35 +1,47 @@
-const request = require('request');
+const request = require('async-request');
 const currency = require('currency-formatter');
 
-function sendUpdate(c) {
-  console.log('GET MARKET PRICE!!!');
-  let msg = '';
-  request.get({
-      url: 'https://api.coinmarketcap.com/v1/ticker/NEO/?convert=USD',
-      json: true
-    },
-    function (e, r, prices) {
-      request.get({
-          url: 'https://api.coinmarketcap.com/v1/ticker/GAS/?convert=USD',
-          json: true
-        },
-        function (err, res, gasPrices) {
-          if(!prices || !gasPrices) return false;
-          const price = prices[0];
-          const gasPrice = gasPrices[0];
-          
-          console.log('Price NEO: ', price.price_usd);
-          console.log('Price GAS: ', gasPrice.price_usd);
-          const gasToNeoPriceRatio = (gasPrice.price_usd / price.price_usd * 100).toFixed(2);
-          msg += `NEO ${currency.format(price.price_usd, {code: 'USD'})} (B${price.price_btc}) - GAS ${currency.format(gasPrice.price_usd, {code: 'USD'})} (B${gasPrice.price_btc}) - G/N RATIO: ${gasToNeoPriceRatio}%`;
-          c.send(msg);
-        });
-    });
+async function sendUpdate(c) {
+  try {
+    console.log('GET MARKET PRICE!!!');
+  
+    const neoUSDPriceRaw = await request('https://binance.com/api/v1/ticker/24hr?symbol=NEOUSDT');
+  
+    const neoBTCPriceRaw = await request('https://binance.com/api/v1/ticker/24hr?symbol=NEOBTC');
+  
+    const gasBTCPriceRaw = await request('https://binance.com/api/v1/ticker/24hr?symbol=GASBTC');
+  
+    const btcUSDPriceRaw = await request('https://binance.com/api/v1/ticker/24hr?symbol=BTCUSDT');
+  
+    const neoUSDPrice = JSON.parse(neoUSDPriceRaw.body);
+  
+    const neoBTCPrice = JSON.parse(neoBTCPriceRaw.body);
+  
+    const gasBTCPrice = JSON.parse(gasBTCPriceRaw.body);
+  
+    const btcUSDPrice = JSON.parse(btcUSDPriceRaw.body);
+    
+    
+    const gasUSDPrice = parseFloat(gasBTCPrice.lastPrice) * parseFloat(btcUSDPrice.lastPrice);
+    const gasToNeoPriceRatio = (gasUSDPrice / neoUSDPrice.lastPrice * 100).toFixed(2);
+  
+    let msg = '';
+    msg += `NEO ${currency.format(neoUSDPrice.lastPrice, {code: 'USD'})} (B${neoBTCPrice.lastPrice})`;
+    msg += ` - GAS ${currency.format(gasUSDPrice, {code: 'USD'})} (B${gasBTCPrice.lastPrice})`;
+    msg += ` - G/N RATIO: ${gasToNeoPriceRatio}%`;
+    msg += ` - B ${currency.format(btcUSDPrice.lastPrice, {code: 'USD'})}`;
+  
+    console.log(msg);
+  
+    c.send(msg);
+  } catch(e) {
+    console.log('ERROR', e.message);
+  }
 }
 
-module.exports = (channel) => {
-  sendUpdate(channel);
-  setInterval((c) => {
-    sendUpdate(c);
-  }, 300000, channel);
+module.exports = async (channel) => {
+  await sendUpdate(channel);
+  setInterval(async (c) => {
+    await sendUpdate(c);
+  }, 200000, channel);
 };
