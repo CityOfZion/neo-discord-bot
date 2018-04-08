7744b27f2dcc2c1a06941684a4ed93d773131d17
Michael Siedlecki <msiedlecki@gmail.com>
2018-04-07 19:06:55 -0700

    Include ONT price in #market price updates

--- imports/market-price-updates.js
+++ imports/market-price-updates.js
@@ -10,6 +10,8 @@ async function sendUpdate(c) {
     const neoBTCPriceRaw = await request('https://binance.com/api/v1/ticker/24hr?symbol=NEOBTC');
   
     const gasBTCPriceRaw = await request('https://binance.com/api/v1/ticker/24hr?symbol=GASBTC');
+
+    const ontBTCPriceRaw = await request('https://binance.com/api/v1/ticker/24hr?symbol=ONTBTC');
   
     const btcUSDPriceRaw = await request('https://binance.com/api/v1/ticker/24hr?symbol=BTCUSDT');
   
@@ -19,16 +21,21 @@ async function sendUpdate(c) {
   
     const gasBTCPrice = JSON.parse(gasBTCPriceRaw.body);
   
+    const ontBTCPrice = JSON.parse(ontBTCPriceRaw.body);
+  
     const btcUSDPrice = JSON.parse(btcUSDPriceRaw.body);
     
-    
     const gasUSDPrice = parseFloat(gasBTCPrice.lastPrice) * parseFloat(btcUSDPrice.lastPrice);
+    const ontUSDPrice = parseFloat(ontBTCPrice.lastPrice) * parseFloat(btcUSDPrice.lastPrice);
     const gasToNeoPriceRatio = (gasUSDPrice / neoUSDPrice.lastPrice * 100).toFixed(2);
+    const ontToNeoPriceRatio = (ontUSDPrice / neoUSDPrice.lastPrice * 100).toFixed(2);
   
     let msg = '';
     msg += `NEO ${currency.format(neoUSDPrice.lastPrice, {code: 'USD'})} (B${neoBTCPrice.lastPrice})`;
     msg += ` - GAS ${currency.format(gasUSDPrice, {code: 'USD'})} (B${gasBTCPrice.lastPrice})`;
     msg += ` - G/N RATIO: ${gasToNeoPriceRatio}%`;
+    msg += ` - ONT ${currency.format(ontUSDPrice, {code: 'USD'})} (B${ontBTCPrice.lastPrice})`;
+    msg += ` - O/N RATIO: ${ontToNeoPriceRatio}%`;
     msg += ` - B ${currency.format(btcUSDPrice.lastPrice, {code: 'USD'})}`;
   
     console.log(msg);
