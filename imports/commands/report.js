0591afe772b0ca204058bbb6f14ba52d2accd2ae
Jakub Mucha <biuro@muchastudio.com.pl>
2018-04-19 19:05:57 +0200

    report command updated to commando format

--- imports/commands/report.js
+++ imports/commands/report.js
@@ -1,3 +1,5 @@
+const { Command } = require('discord.js-commando');
+
 const userHasReportedBefore = (reports, userId) => {
   let reported = false;
   reports.forEach(report => {
@@ -40,100 +42,113 @@ const reportAction = (userJson, reportsNeeded, targetUser, reportChannel) => {
   }
 };
 
-module.exports = function (client, message) {
-  try {
-    const settings = require('../../settings');
-    const fs = require('fs');
-    
-    const reportsNeeded = {kick: 3, ban3days: 4, ban: 5};
-    
-    const [command, user, ...reason] = message.content.split(' ').filter((str) => str);
-    const targetUser = message.mentions.members.first();
-    const reportChannel = client.channels.get(settings.reportChannel);
-    const fileName = `./userdata/${targetUser.id}.json`;
-    
-    
-    // Check for protected roles
-    if (targetUser.roles.some(r => [
-        "CoZ_Council",
-        "NEO_China",
-        "CoZ_Dev",
-        "Community_Mod",
-        "Security",
-        "CoZ",
-        "Meetup_Speaker",
-        "Comms",
-        "Translator",
-        "NNT",
-        "Marketing",
-        "Consensus_node_host",
-        "Hackathon",
-        "CoZ_Strategy"
-      ].includes(r.name))) {
-      console.log('User was report but has a special role');
-      return;
-    }
-    
-    // Check if a reason is specified
-    if (reason.length === 0) {
-      return;
-    }
-    
-    // The current report
-    const report = {
-      date: new Date(),
-      reason: reason.join(' '),
-      by: message.author.id
-    };
-    
-    // Check if the user JSON exist
-    fs.access(fileName, err => {
-      if (!err || err.code !== 'ENOENT') {
-        // Open user JSON
-        fs.readFile(fileName, (err, fd) => {
-          if (err) {
-            console.log('readFile', err);
-          } else {
-            const userJson = JSON.parse(fd);
-            
-            // Check if user has been reported before
-            if (userJson.reports) {
-              
-              if (!userHasReportedBefore(userJson.reports, message.author.id)) {
-                userJson.reports.push(report);
-                
-                reportChannel.send(`${targetUser.toString()} has been reported by **${message.author.toString()} for ${reason && reason.length > 0 ? reason.join(' ') : 'no reason specified'}** and has been reported **${userJson.reports.length}** times now`);
-                
-                reportAction(userJson, reportsNeeded, targetUser, reportChannel);
-              }
-            } else {
-              userJson.reports = [report];
-            }
-            
-            // Save the new data
-            fs.writeFile(fileName, JSON.stringify(userJson), err => {
-              if (err) console.log('error writing to file ' + fileName);
-            });
-          }
-        });
-      } else {
-        reportChannel.send(`${targetUser.toString()} has been reported by ${message.author.toString()} for **${reason && reason.length > 0 ? reason.join(' ') : 'no reason specified'}**`);
-        
-        // Initial data
-        const data = {
-          reports: [
-            report
-          ]
-        };
-        
-        fs.writeFile(fileName, JSON.stringify(data), err => {
-          if (err) console.log('error writing to file ' + fileName);
-        });
+module.exports = class ReportCommand extends Command {
+  constructor(client) {
+      super(client, {
+          name: 'report',
+          group: 'general',
+          memberName: 'report',
+          description: 'Reports improper behaviour of specific user.',
+          examples: ['!report <user> <reason>'],
+          guildOnly: false,
+      });
+  }
+
+  async run(message) {
+    try {
+      const settings = require('../../settings');
+      const fs = require('fs');
+      
+      const reportsNeeded = {kick: 3, ban3days: 4, ban: 5};
+      
+      const [command, user, ...reason] = message.content.split(' ').filter((str) => str);
+      const targetUser = message.mentions.members.first();
+      const reportChannel = client.channels.get(settings.reportChannel);
+      const fileName = `./userdata/${targetUser.id}.json`;
+      
+      
+      // Check for protected roles
+      if (targetUser.roles.some(r => [
+          "CoZ_Council",
+          "NEO_China",
+          "CoZ_Dev",
+          "Community_Mod",
+          "Security",
+          "CoZ",
+          "Meetup_Speaker",
+          "Comms",
+          "Translator",
+          "NNT",
+          "Marketing",
+          "Consensus_node_host",
+          "Hackathon",
+          "CoZ_Strategy"
+        ].includes(r.name))) {
+        console.log('User was report but has a special role');
+        return;
       }
-    });
-  } catch (e) {
-    console.log('------REPORT ERROR------');
-    console.log(e.message);
-    console.log('------REPORT ERROR END------');
+      
+      // Check if a reason is specified
+      if (reason.length === 0) {
+        return;
+      }
+      
+      // The current report
+      const report = {
+        date: new Date(),
+        reason: reason.join(' '),
+        by: message.author.id
+      };
+      
+      // Check if the user JSON exist
+      fs.access(fileName, err => {
+        if (!err || err.code !== 'ENOENT') {
+          // Open user JSON
+          fs.readFile(fileName, (err, fd) => {
+            if (err) {
+              console.log('readFile', err);
+            } else {
+              const userJson = JSON.parse(fd);
+              
+              // Check if user has been reported before
+              if (userJson.reports) {
+                
+                if (!userHasReportedBefore(userJson.reports, message.author.id)) {
+                  userJson.reports.push(report);
+                  
+                  reportChannel.send(`${targetUser.toString()} has been reported by **${message.author.toString()} for ${reason && reason.length > 0 ? reason.join(' ') : 'no reason specified'}** and has been reported **${userJson.reports.length}** times now`);
+                  
+                  reportAction(userJson, reportsNeeded, targetUser, reportChannel);
+                }
+              } else {
+                userJson.reports = [report];
+              }
+              
+              // Save the new data
+              fs.writeFile(fileName, JSON.stringify(userJson), err => {
+                if (err) console.log('error writing to file ' + fileName);
+              });
+            }
+          });
+        } else {
+          reportChannel.send(`${targetUser.toString()} has been reported by ${message.author.toString()} for **${reason && reason.length > 0 ? reason.join(' ') : 'no reason specified'}**`);
+          
+          // Initial data
+          const data = {
+            reports: [
+              report
+            ]
+          };
+          
+          fs.writeFile(fileName, JSON.stringify(data), err => {
+            if (err) console.log('error writing to file ' + fileName);
+          });
+        }
+      });
+    } catch (e) {
+      console.log('------REPORT ERROR------');
+      console.log(e.message);
+      console.log('------REPORT ERROR END------');
+    }
   }
 };
