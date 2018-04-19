5722555e760eaf15052f6654171a9b959ab88787
Jakub Mucha <biuro@muchastudio.com.pl>
2018-04-19 16:32:07 +0200

    neo command updated to commando format

--- imports/commands/neo.js
+++ imports/commands/neo.js
@@ -1,13 +1,27 @@
+const { Command } = require('discord.js-commando');
 const request = require('request');
 const currency = require('currency-formatter');
 
-module.exports = (client, message) => {
-  request.get({
-      url: 'https://api.coinmarketcap.com/v1/ticker/neo/?convert=USD',
-      json: true
-    },
-    function (e, r, prices) {
-      const price = prices[0];
-      message.channel.send(`current NEO price = ${currency.format(price.price_usd, { code: 'USD' })}, B${price.price_btc} (CoinMarketCap)`);
-    });
+module.exports = class NeoCommand extends Command {
+  constructor(client) {
+      super(client, {
+          name: 'neo',
+          group: 'neo-related',
+          memberName: 'neo',
+          description: 'Shows more details for NEO using CoinMarketCap.',
+          examples: ['!neo'],
+          guildOnly: false,
+      });
+  }
+
+  async run(message) {
+    request.get({
+        url: 'https://api.coinmarketcap.com/v1/ticker/neo/?convert=USD',
+        json: true
+      },
+      function (e, r, prices) {
+        const price = prices[0];
+        message.channel.send(`current NEO price = ${currency.format(price.price_usd, { code: 'USD' })}, B${price.price_btc} (CoinMarketCap)`);
+      });
+  }
 };
