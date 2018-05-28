33fb0813155fd92bbfd072459ae9cc5a963477f7
Jakub Mucha <biuro@muchastudio.com.pl>
2018-05-28 23:37:58 +0200

    fixed linting for all commands

--- imports/commands/neo-related/neo.js
+++ imports/commands/neo-related/neo.js
@@ -4,24 +4,30 @@ const currency = require('currency-formatter');
 
 module.exports = class NeoCommand extends Command {
   constructor(client) {
-      super(client, {
-          name: 'neo',
-          group: 'neo-related',
-          memberName: 'neo',
-          description: 'Shows more details for NEO using CoinMarketCap.',
-          examples: ['!neo'],
-          guildOnly: false,
-      });
+    super(client, {
+      name: 'neo',
+      group: 'neo-related',
+      memberName: 'neo',
+      description: 'Shows more details for NEO using CoinMarketCap.',
+      examples: ['!neo'],
+      guildOnly: false
+    });
   }
 
   async run(message) {
-    request.get({
+    request.get(
+      {
         url: 'https://api.coinmarketcap.com/v1/ticker/neo/?convert=USD',
         json: true
       },
-      function (e, r, prices) {
+      function(e, r, prices) {
         const price = prices[0];
-        message.channel.send(`current NEO price = ${currency.format(price.price_usd, { code: 'USD' })}, B${price.price_btc} (CoinMarketCap)`);
-      });
+        message.channel.send(
+          `current NEO price = ${currency.format(price.price_usd, { code: 'USD' })}, B${
+            price.price_btc
+          } (CoinMarketCap)`
+        );
+      }
+    );
   }
 };
