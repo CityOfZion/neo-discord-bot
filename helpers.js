4df94ccf1c401765bbb64de089485265adbc5d55
Jakub Mucha <biuro@muchastudio.com.pl>
2018-05-28 23:38:10 +0200

    fixed linting for helpers and settings

--- helpers.js
+++ helpers.js
@@ -1,9 +1,10 @@
 module.exports = {
-    deleteMsg: (message) => {
-        if (message.channel.type !== 'dm') {
-            message.delete()
-            .then(msg => console.log(`Deleted message from ${msg.author}`))
-            .catch(console.error);
-        }
-    },
-}
+  deleteMsg: message => {
+    if (message.channel.type !== 'dm') {
+      message
+        .delete()
+        .then(msg => console.log(`Deleted message from ${msg.author}`))
+        .catch(console.error);
+    }
+  }
+};
--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 module.exports = {
-    botToken: '<PUT_TOKEN_HERE>',
-    marketPriceChannel: '<PUT_CHANNEL_ID_HERE>',
-    reportChannel: '<PUT_CHANNEL_ID_HERE>',
-    ownersId: '<PUT_MEMBER_ID_HERE>', // example: 'ID' (string) or ['ID1', 'ID2'] (array)
-    botPrefix: '<PUT_BOT_PREFIX_HERE>', // example: '!'
-    autoReconnect: true,
-    unknownCommandResponse: false, // if unknown command was triggered, let user know about it
-    disableEveryone: true, // bot will never trigger @everyone
+  botToken: '<PUT_TOKEN_HERE>',
+  marketPriceChannel: '<PUT_CHANNEL_ID_HERE>',
+  reportChannel: '<PUT_CHANNEL_ID_HERE>',
+  ownersId: '<PUT_MEMBER_ID_HERE>', // example: 'ID' (string) or ['ID1', 'ID2'] (array)
+  botPrefix: '<PUT_BOT_PREFIX_HERE>', // example: '!'
+  autoReconnect: true,
+  unknownCommandResponse: false, // if unknown command was triggered, let user know about it
+  disableEveryone: true // bot will never trigger @everyone
 };
