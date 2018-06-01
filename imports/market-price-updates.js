d234077c8146994144367d056ab00ef0a862fdc2
Jakub Mucha <biuro@muchastudio.com.pl>
2018-06-01 22:52:48 +0200

    added smart price update feature

--- imports/market-price-updates.js
+++ imports/market-price-updates.js
@@ -1,8 +1,27 @@
 const axios = require('axios');
 const currency = require('currency-formatter');
+const { marketPriceCommand } = require('../settings');
+const {
+  incrementPriceChannelMsgCount,
+  getPriceChannelMsgCount,
+  resetPriceChannelMsgCount
+} = require('../helpers');
 
 async function sendUpdate(c) {
   try {
+    const lastPriceChannelMsgs = await c.fetchMessages({
+      limit: marketPriceCommand.smartPriceMessagesThreshold
+    });
+
+    await lastPriceChannelMsgs.every(msg => {
+      if (!msg.content.includes(marketPriceCommand.uniqueStringOfCommand)) {
+        incrementPriceChannelMsgCount();
+        return true;
+      } else {
+        return false;
+      }
+    });
+
     console.log('GET MARKET PRICE!!!');
 
     const neoUSDPriceRaw = await axios('https://binance.com/api/v1/ticker/24hr?symbol=NEOUSDT');
@@ -18,19 +37,38 @@ async function sendUpdate(c) {
     const gasUSDPrice = parseFloat(gasBTCPrice.lastPrice) * parseFloat(btcUSDPrice.lastPrice);
     const gasToNeoPriceRatio = ((gasUSDPrice / neoUSDPrice.lastPrice) * 100).toFixed(2);
 
-    let msg = '';
-    msg += `NEO ${currency.format(neoUSDPrice.lastPrice, { code: 'USD' })} (B${
+    let msgToSend = '';
+    msgToSend += `NEO ${currency.format(neoUSDPrice.lastPrice, { code: 'USD' })} (B${
       neoBTCPrice.lastPrice
     })`;
-    msg += ` - GAS ${currency.format(gasUSDPrice, { code: 'USD' })} (B${gasBTCPrice.lastPrice})`;
-    msg += ` - G/N RATIO: ${gasToNeoPriceRatio}%`;
-    msg += ` - B ${currency.format(btcUSDPrice.lastPrice, { code: 'USD' })}`;
+    msgToSend += ` - GAS ${currency.format(gasUSDPrice, { code: 'USD' })} (B${
+      gasBTCPrice.lastPrice
+    })`;
+    msgToSend += ` - G/N RATIO: ${gasToNeoPriceRatio}%`;
+    msgToSend += ` - B ${currency.format(btcUSDPrice.lastPrice, { code: 'USD' })}`;
 
-    console.log(msg);
-
-    c.send(msg);
+    if (getPriceChannelMsgCount() >= marketPriceCommand.smartPriceMessagesThreshold) {
+      const sentMessage = await c.send(msgToSend);
+      console.log(`Market price sent with new message: ${sentMessage.content}`);
+      resetPriceChannelMsgCount();
+    } else {
+      const lastMsgs = await c.fetchMessages({
+        limit: marketPriceCommand.smartPriceMessagesThreshold
+      });
+      await lastMsgs.every(async msg => {
+        if (msg.author.bot && msg.content.includes(marketPriceCommand.uniqueStringOfCommand)) {
+          const editedMsg = await msg.edit(msgToSend);
+          console.log(`Market price updated in previous message: ${editedMsg.content}`);
+          resetPriceChannelMsgCount();
+          return false;
+        } else {
+          return true;
+        }
+      });
+    }
   } catch (e) {
     console.log('ERROR', e.message);
+    await resetPriceChannelMsgCount();
   }
 }
 
@@ -40,7 +78,7 @@ module.exports = async channel => {
     async c => {
       await sendUpdate(c);
     },
-    200000,
+    marketPriceCommand.updateInterval,
     channel
   );
 };
