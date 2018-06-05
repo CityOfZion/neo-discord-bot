0129eceee874a670bb38dcf6d43ab8d569bdff1c
Jakub Mucha <biuro@muchastudio.com.pl>
2018-06-05 14:31:19 +0200

    added helpers for fetching/setting/retrieval and setup of coin dictionary

--- helpers.js
+++ helpers.js
@@ -1,6 +1,10 @@
-let priceChannelMessageCount = 0;
+const axios = require('axios');
+const { coinDictUpdateInterval, coinDictUrl } = require('./settings');
 
-module.exports = {
+let priceChannelMessageCount = 0;
+let coinDictionary = null;
+
+const helpers = {
   deleteMsg: async message => {
     if (message.channel.type !== 'dm') {
       try {
@@ -19,5 +23,43 @@ module.exports = {
   },
   resetPriceChannelMsgCount: () => {
     priceChannelMessageCount = 0;
+  },
+  fetchCoinDict: async () => {
+    try {
+      const listings = await axios(coinDictUrl);
+      const coins = listings.data.data.map(coin => {
+        /* eslint-disable-next-line camelcase */
+        const { id, symbol, name } = coin;
+        return { id, symbol, name };
+      });
+      helpers.setCoinDict(coins);
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  setCoinDict: newDict => {
+    if (newDict && newDict.length && coinDictionary !== newDict) {
+      coinDictionary = newDict;
+      console.log(
+        `Coin dictionary has been updated successfuly - coins count: ${coinDictionary.length}`
+      );
+    } else if (newDict && !newDict.length) {
+      console.log(`New dictionary didn't contain any coins, not updating`);
+    } else if (!newDict) {
+      console.log('New dictionary looks empty, not updating');
+    } else {
+      console.log("New dictionary didn't change, not updating");
+    }
+  },
+  getCoinDict: () => {
+    return coinDictionary;
+  },
+  setupCoinDict: async () => {
+    await helpers.fetchCoinDict();
+    setInterval(async () => {
+      await helpers.fetchCoinDict();
+    }, coinDictUpdateInterval);
   }
 };
+
+module.exports = helpers;
