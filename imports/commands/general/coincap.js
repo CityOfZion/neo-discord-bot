5afda564899b03528e2dfe7e1c9152732e1cbd55
Jakub Mucha <biuro@muchastudio.com.pl>
2018-06-05 22:12:21 +0200

    coincap refactor, using axios now

--- imports/commands/general/coincap.js
+++ imports/commands/general/coincap.js
@@ -1,5 +1,5 @@
 const { Command } = require('discord.js-commando');
-const request = require('request');
+const axios = require('axios');
 const currency = require('currency-formatter');
 const { getCoin } = require.main.require('./helpers');
 
@@ -30,41 +30,41 @@ module.exports = class CoincapCommand extends Command {
       return;
     }
 
-    request.get(
-      {
-        url: `https://api.coinmarketcap.com/v1/ticker/${requestedCoin.website_slug}/?convert=USD`,
-        json: true
-      },
-      function(e, r, data) {
-        if (Object.keys(data).length === 0) {
-          message.channel.send('Unable to find provided coin');
-          return;
-        }
+    try {
+      const coinTicker = await axios(
+        `https://api.coinmarketcap.com/v1/ticker/${requestedCoin.website_slug}/?convert=USD`
+      );
 
-        if (!data || !data[0] || !data[0]['id']) {
-          return;
-        }
-
-        const priceBtc = data[0].price_btc;
-        const percentChange24h = data[0].percent_change_24h;
-        const name = data[0].name;
-        const id = data[0].id;
-        const marketCapUsd = data[0].market_cap_usd;
-        const priceUsd = data[0].price_usd;
-        const maxSupply = data[0].max_supply;
-
-        message.channel.send(
-          `${name} (${id})\n${currency.format(priceUsd, {
-            code: 'USD'
-          })} (${priceBtc})  +/-: ${percentChange24h}%  Volume: ${currency.format(
-            data[0]['24h_volume_usd'],
-            { code: 'USD' }
-          )}\nSupply: ${currency.format(maxSupply, {})}  Market Cap: ${currency.format(
-            marketCapUsd,
-            { code: 'USD' }
-          )}`
-        );
+      if (!coinTicker || !coinTicker.data[0]) {
+        message.channel.send('Unable to find provided coin');
+        return;
       }
-    );
+
+      const coinData = coinTicker.data[0];
+
+      const priceBtc = coinData.price_btc;
+      const percentChange24h = coinData.percent_change_24h;
+      const volumeUsd24h = coinData['24h_volume_usd'];
+      const name = coinData.name;
+      const id = coinData.id;
+      const marketCapUsd = coinData.market_cap_usd;
+      const priceUsd = coinData.price_usd;
+      const maxSupply = coinData.max_supply;
+
+      message.channel.send(
+        `${name} (${id})\n${currency.format(priceUsd, {
+          code: 'USD'
+        })} (${priceBtc})  +/-: ${percentChange24h}%  Volume: ${currency.format(volumeUsd24h, {
+          code: 'USD'
+        })}\nSupply: ${currency.format(maxSupply, {})}  Market Cap: ${currency.format(
+          marketCapUsd,
+          {
+            code: 'USD'
+          }
+        )}`
+      );
+    } catch (err) {
+      console.error(err);
+    }
   }
 };
